4c92fb8976f9cf264cea6f550ee39e41391341a6
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-10 21:00:30 -0300

    feat: add getroles() for each project

--- main.go
+++ main.go
@@ -1,70 +1,66 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-
-	"cloud.google.com/go/pubsub"
-	"google.golang.org/api/option"
+	"os"
 )
 
-// apiKey shows how to use an API key to authenticate.
-func apiKey() error {
-	client, err := pubsub.NewClient(context.Background(), "PROJECT_ID", option.WithAPIKey("TOKEN"))
-	if err != nil {
-		return fmt.Errorf("pubsub.NewClient: %v", err)
-	}
-	defer client.Close()
-	// Use the authenticated client.
-	_ = client
-
-	return nil
-}
-
-func getACL() error {
-	client, err := pubsub.NewClient(context.Background(), "eng-venture-320213", option.WithAPIKey("AIzaSyBOgoKN8ab-LloliWG6H-lXAqvTTmkyb2A"))
-	if err != nil {
-		return fmt.Errorf("pubsub.NewClient: %v", err)
-	}
-	defer client.Close()
-	// Use the authenticated client.
-	_ = client
-
-	return nil
-}
-
-type Response struct {
-	Name        string `json:"name"`
-	DisplayName string `json:"displayName"`
-	Description string `json:"description"`
-	State       string `json:"state"`
-	Disabled    string `json:"disabled"`
-}
-
 func main() {
-	apiKey()
+	GetRoles()
+}
 
-	fmt.Println("Calling API...")
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://iam.googleapis.com/v1beta/{parent=projects/*/locations/*}/workloadIdentityPools", nil)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
+type Roles struct {
+	Role []struct {
+		Name        string `json:"name"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+		Etag        string `json:"etag"`
+	} `json:"roles"`
+}
+
+
+// GetRoles POST and return JSON with all roles of project, need export GCP_API_KEY and PROJECT_ID to work
+// GCP_API_KEY you can obtain with "gcloud auth print-access-token"
+func GetRoles() {
+
+	token := os.Getenv("GCP_API_KEY")
+	project := os.Getenv("PROJECT_ID")
+	url := fmt.Sprintf("https://iam.googleapis.com/v1/projects/%s/roles", project)
+
+	bearer := "Bearer " + token
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(nil))
+	req.Header.Set("Authorization", bearer)
 	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		for key, val := range via[0].Header {
+			req.Header[key] = val
+		}
+		return err
+	}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Print(err.Error())
+		log.Println("Error on response.\n[ERRO] -", err)
+	} else {
+		defer resp.Body.Close()
+		var roles Roles
+		data, _ := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(data, &roles)
+
+		for i := 0; i < 2; i++{
+			fmt.Println("Path:", roles.Role[i].Name)
+			fmt.Println("Name:", roles.Role[i].Title)
+			fmt.Println("Description:", roles.Role[i].Description)
+			fmt.Println("---")
+		}
 	}
-	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-	var responseObject Response
-	json.Unmarshal(bodyBytes, &responseObject)
-	fmt.Printf("API Response as struct %+v\n", responseObject)
 }
